35863869fd02012fa6c3b5a3b651e54a1c924aac
ZainabNaeem10 <zainabnaeem1004@gmail.com>
2026-03-20 03:03:42 +0500

    Rm import script

--- config.php
+++ config.php
@@ -1,21 +1,7 @@
 <?php
-$url = getenv('DATABASE_URL');
+$conn=mysqli_connect("localhost","root","","medical_store");
 
-if(!$url){
-    die("DATABASE_URL environment variable not found!");
-}
-
-$parts = parse_url($url);
-
-$host = $parts['host'];
-$port = $parts['port'];
-$user = $parts['user'];
-$pass = $parts['pass'];
-$db   = ltrim($parts['path'], '/');
-
-$conn = mysqli_connect($host, $user, $pass, $db, $port);
-
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+if(!$conn){
+die("Database Connection Failed");
 }
 ?>
